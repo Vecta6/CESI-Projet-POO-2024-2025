055c0aa6850eab39971e5284a5fb836a0002fffe
Nathan <nathan.guerton@viacesi.fr>
2024-12-03 14:39:37 +0100

    commentaire inutile enlevé

--- main.cpp
+++ main.cpp
@@ -15,34 +15,6 @@ const int gridHeight = 200;
 
 Grille grille(gridWidth, gridHeight, cellSize);
 
-// vector<vector<int>> grid(gridWidth, vector<int>(gridHeight));
-
-// void initializeGrid() {
-//     srand(time(0));
-//     for (int x = 0; x < gridWidth; ++x) {
-//         for (int y = 0; y < gridHeight; ++y) {
-//             grid[x][y] = rand() % 2;
-//         }
-//     }
-// }
-
-// void renderGrid(sf::RenderWindow &window) {
-//     int x, y;
-    
-//     window.clear();
-//     sf::RectangleShape cell(sf::Vector2f(cellSize - 1.0f, cellSize - 1.0f));
-//     for (x = 0; x < gridWidth; ++x) {
-//         for (y = 0; y < gridHeight; ++y) {
-
-//             if (grid[x][y] == 1) {
-//                 cell.setPosition(x * cellSize, y * cellSize);
-//                 window.draw(cell);
-//             }
-//         }
-//     }
-//     window.display();
-// }
-
 int main() {
     sf::RenderWindow window(sf::VideoMode(gridWidth * cellSize, gridHeight * cellSize), "Game of Life");
     
